e4a75902006e58d80d9d321333706662a36fc5ed
SAIYAM GUPTA <saiyamsomya@gmail.com>
2025-09-12 17:15:38 +0530

    Hello World! in JAVA

--- Basics/first.java
+++ Basics/first.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+
 public class first {
+
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Hello, World!");
