8a751f5aeed6ff5159dd183701e0a408543454ab
SAIYAM GUPTA <saiyamsomya@gmail.com>
2025-09-13 12:10:57 +0530

    Print Characters with ASCII values

---
+++ Loops/AsciiChars.java
@@ -0,0 +1,7 @@
+public class AsciiChars {
+    public static void main(String[] args) {
+        for (int i = 65; i <= 90; i++) {
+            System.out.println((char)i + " " + i);
+        }
+    }
+}
