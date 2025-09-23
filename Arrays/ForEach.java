4e3ba810b62111215acc8b0fff8fe5d4563250ba
SAIYAM GUPTA <saiyamsomya@gmail.com>
2025-09-23 18:44:32 +0530

    For Each Loop in Java

---
+++ Arrays/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+        int[] arr = {1,3,3,6,8,5,2,9};
+
+        for(int ele : arr){
+            System.out.print(ele + " ");
+        }
+    }
+}
