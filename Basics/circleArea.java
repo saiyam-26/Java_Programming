2c776b6db1e834eaa46a58a3773fdbeea73ebc43
SAIYAM GUPTA <saiyamsomya@gmail.com>
2026-01-15 10:17:52 +0530

    Solution to Area of a Circle using JAVA

--- Basics/circleArea.java
+++ Basics/circleArea.java
@@ -1,16 +1,15 @@
-import java.util.Scanner;
-public class circleArea{
+import java.util.*;
+public class CircleArea{
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
 
-        
         System.out.print("Enter radius: ");
         double radius = sc.nextDouble();
         
         double pi = 3.1415;
-        
         double area = pi * radius * radius;
+        
         System.out.println("Area = " + area);
     }
 }
