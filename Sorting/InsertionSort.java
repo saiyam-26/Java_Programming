7932b48bff42151554770326cb699480a8413452
SAIYAM GUPTA <saiyamsomya@gmail.com>
2026-01-18 10:43:22 +0530

    Uncomment Insertion Sort implementation

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,10 +1,10 @@
-		// for(int i = 1; i < arr.length; i++){
-		//     int j = i;
+		for(int i = 1; i < arr.length; i++){
+		    int j = i;
 		    
-		//     while(j > 0 && arr[j] < arr[j - 1]){
-		//         int temp = arr[j];
-		//         arr[j] = arr[j - 1];
-		//         arr[j - 1] = temp;
-		//         j--;
-		//     }
-		// }
+		    while(j > 0 && arr[j] < arr[j - 1]){
+		        int temp = arr[j];
+		        arr[j] = arr[j - 1];
+		        arr[j - 1] = temp;
+		        j--;
+		    }
+		}
