18eeec90816aefec2857938e59a9a48b011be361
SAIYAM GUPTA <saiyamsomya@gmail.com>
2026-01-19 18:22:51 +0530

    Diamond Pattern

---
+++ Patterns/Diamond.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Diamond {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the No. of Rows : ");
+        int n = sc.nextInt();
+        System.out.println();
+        
+        // upper half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n + 4; j++) {
+                if (j > n + 1 - i && j < n - 1 + i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+        // lower half
+        for (int i = 1; i <= n - 1; i++) {
+            for (int j = 1; j <= n + 4; j++) {
+                if (j > i + 1 && j < n + 4 - i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+        sc.close();
+    }
+}
+
+/*
+Example Output if n = 5:
+
+Enter the No. of Rows : 5
+
+    *    
+   ***   
+  *****  
+ ******* 
+*********
+ ******* 
+  *****  
+   ***   
+    *    
+*/
