81989c4c237d16e97e4822db9a36b60c35feef18
SAIYAM GUPTA <saiyamsomya@gmail.com>
2026-01-19 18:04:41 +0530

    Square Of Alphabets

---
+++ Patterns/AlphabetSquare.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class AlphabetSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the No. of Rows : ");
+        int n = sc.nextInt();
+        System.out.println();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (true) {
+                    System.out.print((char)(j + 64));
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
+/*
+Example for n = 4:
+ABCD
+ABCD
+ABCD
+ABCD
+*/
