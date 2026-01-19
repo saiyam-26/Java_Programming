cf5d01bde560799b3fab3fd00df92cf55f2ae335
SAIYAM GUPTA <saiyamsomya@gmail.com>
2026-01-19 17:54:32 +0530

    Print Table of 17 using 2 methods

---
+++ Loops/TableOf17.java
@@ -0,0 +1,36 @@
+class class TableOf17 {
+    public static void main(String[] args) {
+
+        System.out.println("Method 1: Using i % 17 == 0");
+        for (int i = 1; i <= 170; i++) {
+            if (i % 17 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println();
+
+        System.out.println("Method 2: Using i += 17");
+        for (int i = 17; i <= 170; i += 17) {
+            System.out.print(i + " ");
+        }
+    }
+}
+ {
+    public static void main(String[] args) {
+
+        System.out.println("Method 1: Using i % 17 == 0");
+        for (int i = 1; i <= 170; i++) {
+            if (i % 17 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println();
+
+        System.out.println("Method 2: Using i += 17");
+        for (int i = 17; i <= 170; i += 17) {
+            System.out.print(i + " ");
+        }
+    }
+}
