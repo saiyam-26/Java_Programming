d10ebd2bf3738ecdf7561c8336af85528e85ea01
SAIYAM GUPTA <saiyamsomya@gmail.com>
2026-02-14 10:35:11 +0530

    Hollow Square
    This program prints a hollow square pattern based on user input for the number of rows.

---
+++ Patterns/HollowSquare.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class HollowSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the No. of Rows : ");
+        int n = sc.nextInt();
+        System.out.println();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
+/*
+Example for n = 5:
+
+*****
+*   *
+*   *
+*   *
+*****
+
+*/
