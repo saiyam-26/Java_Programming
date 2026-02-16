af680a3a25d65c1e9c431cd6ca3e5adb18c9ed09
SAIYAM GUPTA <saiyamsomya@gmail.com>
2026-02-16 21:26:14 +0530

    Star Pyramid pattern printing in Pyramid.java
    Implement a Pyramid pattern printing program in Java.

---
+++ Patterns/Pyramid.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the No. of Rows : ");
+        int n = sc.nextInt();
+        System.out.println();
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 4 + n; j++) {
+                if (j >= n + 1 - i && j <= n - 1 + i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+        sc.close();
+    }
+}
+
+/*
+Example Output if n = 5:
+
+Enter the No. of Rows : 5
+
+    *    
+   ***   
+  *****  
+ ******* 
+*********
+*/
